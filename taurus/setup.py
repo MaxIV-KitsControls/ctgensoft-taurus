708612039f034c367078b6458a0a6bedb8682f41
coutinho <coutinho@esrf.fr>
2014-08-26 15:22:16 +0200

    Move TaurusCommandAction to new action submodule

--- taurus/setup.py
+++ taurus/setup.py
@@ -112,6 +112,7 @@ packages = [
 
     'taurus.qt.qtgui',
     'taurus.qt.qtgui.test',
+    'taurus.qt.qtgui.action',
     'taurus.qt.qtgui.application',
     'taurus.qt.qtgui.base',
     'taurus.qt.qtgui.button',
